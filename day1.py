9ac5bfc4a7331d3a23e6a7ac8a894c6076f035db
KellyMK <kellygirl23@gmail.com>
2021-12-06 23:22:37 -0500

    Refactor day 1 + Joe's solutions

--- day1.py
+++ day1.py
@@ -13,6 +13,9 @@ def task1_1(x):
     
     n_vals = len(x)
     increases = [1 if x[i] - x[i-1] > 0 else 0 for i in range(1, n_vals)]
+    # Joe's solution: 
+    # diffs = x[1:] - x[0:-1]
+    # increases = sum(diffs > 0)
     
     return sum(increases)
 
@@ -33,24 +36,21 @@ def task1_2(x):
     
     n_vals = len(x)
     rolling_sum = [sum(x[i:i+3]) for i in range(n_vals-2)]
-    n_sums = len(rolling_sum)
-    increases = [1 if rolling_sum[i] - rolling_sum[i-1] > 0 else 0 for i in range(1, n_sums)]
+    # Joe's solution:
+    # rolling_sum = x[:-2] + x[1:-1] + x[2:]
     
-    return sum(increases)
+    return task1_1(rolling_sum)
 
 ## Get Answer for Task 1.1
 
-day1a_input_file = open("aoc-input/day1_input.txt", "r")
-day1a_input = [int(i) for i in day1a_input_file.readlines()]
+day1_input_file = open("aoc-input/day1_input.txt", "r")
+day1_input = [int(i) for i in day1_input_file.readlines()]
 
 print("Task 1.1")
-print(task1_1(day1a_input))
+print(task1_1(day1_input))
 
 
 ## Get Answer for Task 1.2
 
-day1b_input_file = open("aoc-input/day1_input.txt", "r")
-day1b_input = [int(i) for i in day1b_input_file.readlines()]
-
 print("Task 1.2")
-print(task1_2(day1b_input))
+print(task1_2(day1_input))
