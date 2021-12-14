92fe42fda9815541d30eb4237894277c99b3dd87
KellyMK <kellygirl23@gmail.com>
2021-12-13 22:32:35 -0500

    Day 3 in progress

--- day3.py
+++ day3.py
@@ -4,7 +4,7 @@ def read_day3(path):
     
     # Read in data from file path
     df = pd.read_table(path, sep=' ', header=None, dtype='str')
-    df_test.head[0].str.split('',n=500,expand=True)
+    df = df[0].str.split('',n=500,expand=True)
 
     # Define float NaN to remove empty strings
     nan_value = float("NaN")
@@ -15,3 +15,18 @@ def read_day3(path):
     
     return df.astype(int)
 
+def day3(path):
+    
+    df = read_day3(path)
+    
+    gamma = df.mode()
+    
+    gamma['X'] = 'A'
+    
+    print(df.groupby('X').agg({
+    'name': lambda x: ''.join(x)
+}))
+    
+
+
+day3("aoc-input/day3_test_input.txt")
